1e5ccba473024637eaf7a231dafc34c0e9be5e51
Like <like@ekil.sh>
2022-12-19 13:56:30 +0800

    .

--- index.ts
+++ index.ts
@@ -1,16 +1,12 @@
-import {
-    addDays,
-    assert,
-    differenceInCalendarDays,
-    subMonths,
-} from './deps.ts';
+import { addDays, differenceInCalendarDays, subMonths } from './deps.ts';
 
 const WAGE_DEFAULT = 7500;
 const WAGE_PER_DAY = 288;
 const WAGE_DAYS = 26;
 const DAY = 17;
 
-function main(year: number, month: number, off = 0, fes = 0) {
+function main(month: number, off = 0, fes = 0) {
+    const year = new Date().getFullYear();
     const today = new Date(year, month - 1, DAY);
     const lastMonth = subMonths(1)(today);
     const diff = differenceInCalendarDays(lastMonth, today);
@@ -33,33 +29,33 @@ function main(year: number, month: number, off = 0, fes = 0) {
         day: 'numeric',
     };
 
-    const startDate = (addDays(1)(lastMonth)).toLocaleDateString('en', options);
-    const endDate = today.toLocaleDateString('en', options);
-    const days = (day: number) => day > 1 ? 'days' : 'day';
-    const holidays = fes > 1 ? 'holidays' : 'holiday';
-    const output = `During [${startDate}, ${endDate}] has ${diff} ${
-        days(diff)
-    }, worked ${worked} ${days(worked)}, ${off} ${
-        days(off)
-    } off，${fes} ${holidays}, need to pay ¥${wage}。`;
+    const startDate = (addDays(1)(lastMonth)).toLocaleDateString('zh', options);
+    const endDate = today.toLocaleDateString('zh', options);
+    const output =
+        `\n${startDate}到${endDate}一共有 ${diff} 天，工作 ${worked} 天, 休息 ${off} 天，节假日 ${fes} 天, 工资共 ¥${wage}。`;
 
     console.log(output);
 
     return wage;
 }
 
-const year = parseInt(
-    prompt('Which year right now? (default: 2022)') || '2022',
-);
-const month = parseInt(prompt('Which month right now? (default: 1)') || '1');
-const off = parseInt(
-    prompt('How many days off during this period? (default: 0)') || '0',
-);
-const fes = parseInt(
-    prompt('How many holidays during this period? (default: 0)') || '0',
-);
-main(year, month, off, fes);
+let month = new Date().getMonth() + 1;
+const monthInput = prompt(`现在是几月？（默认：当前月）：`);
 
-Deno.test('year should be a number', () => {
-    assert(typeof year === 'number');
-});
+if (monthInput) {
+    month = parseInt(monthInput);
+    if (month < 1 || month > 12) {
+        console.error('Month must be between 1 and 12.');
+        Deno.exit(1);
+    }
+}
+
+const off = parseInt(
+    prompt('休息了多少天？（默认：0）：') || '0',
+);
+
+const fes = parseInt(
+    prompt('总共有多少天节假日？（默认：0）：') || '0',
+);
+
+main(month, off, fes);
